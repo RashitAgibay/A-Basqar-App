fd884e6ec27f3b9db9f01c71ec6acf1d345475a5
ilyasshomat <ilyas.shomat@gmail.com>
2020-04-20 23:26:29 +0600

    Movement History page is done

--- ERP(attempt2)/Movements/ViewControllers/MovementHistoryVC.swift
+++ ERP(attempt2)/Movements/ViewControllers/MovementHistoryVC.swift
@@ -8,23 +8,28 @@
 
 import UIKit
 
-class MovementHistoryVC: UIViewController {
+class MovementHistoryVC: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
     }
-    */
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell  = collectionView.dequeueReusableCell(withReuseIdentifier: "movementHistoryCell", for: indexPath)  as! MovementHistoryCell
+        
+        
+        
+        return cell
+    }
+
 
 }
