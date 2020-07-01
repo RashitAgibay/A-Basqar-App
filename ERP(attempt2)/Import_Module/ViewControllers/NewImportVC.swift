6e75e5eb84e2d5f7193221218fda25c6faa2eda3
ilyasshomat <ilyas.shomat@gmail.com>
2020-07-01 15:43:40 +0600

    Massive refactoring(New Import design is done)

--- ERP(attempt2)/Import_Module/ViewControllers/NewImportVC.swift
+++ ERP(attempt2)/Import_Module/ViewControllers/NewImportVC.swift
@@ -8,11 +8,59 @@
 
 import UIKit
 
-class NewImportVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+class NewImportVC: UIViewController {
+    
+    @IBOutlet weak var cardView: UIView!
+    @IBOutlet weak var totalSumLabel: UILabel!
+    @IBOutlet weak var contragentNameButton: UIButton!
+    @IBOutlet weak var buyButton: UIButton!
+    @IBOutlet weak var cancelButton: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupUI()
+    }
+    
+    private func setupUI() {
+        
+         cardView.layer.cornerRadius = 10
+         cardView.dropShadow()
+         cardView.layer.backgroundColor = UIColor.white.cgColor
+
+         buyButton.layer.cornerRadius = 15
+         buyButton.dropShadowforButton()
+         
+         cancelButton.layer.cornerRadius = 15
+         cancelButton.dropShadowforButton()
+         
+         contragentNameButton.layer.cornerRadius = 10
+         contragentNameButton.dropShadowforButton()
+
+        
+    }
+    
+    
+    @IBAction func tappedContragentNameButton(_ sender: Any) {
+        
+    }
+    
+    @IBAction func tappedBuyButton(_ sender: Any) {
+        
+    }
+    @IBAction func tappedCancelButton(_ sender: Any) {
+        
+    }
+    
+}
+
+//MARK: - CollectionView workflow
+extension NewImportVC: UICollectionViewDelegate, UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         
-        return 5
+        return 10
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -24,14 +72,4 @@ class NewImportVC: UIViewController, UICollectionViewDelegate, UICollectionViewD
     
     
     
-
-    
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
-
 }
--- ERP(attempt2)/ViewControllers/CollectionViewCells/Import/NewImportCell.swift
+++ ERP(attempt2)/ViewControllers/CollectionViewCells/Import/NewImportCell.swift
@@ -27,6 +27,21 @@ class NewImportCell: UICollectionViewCell {
     
     private func setupCell() {
         
+        self.contentView.layer.cornerRadius = 10
+        self.layer.shadowColor = UIColor.gray.cgColor
+        self.layer.shadowOffset = CGSize(width: 0, height: 0.5)
+        self.layer.shadowOpacity = 0.5
+        self.layer.masksToBounds = false
+        self.layer.shadowPath = UIBezierPath(roundedRect: self.bounds, cornerRadius: self.contentView.layer.cornerRadius).cgPath
+        
+        
+        self.priceCardView.layer.cornerRadius=10
+        self.priceCardView.layer.backgroundColor = UIColor.white.cgColor
+        self.priceCardView.layer.borderWidth = 1
+        self.priceCardView.layer.borderColor = hexStringToUIColor(hex: "#3F639D").cgColor
+        
+        self.productImageView.layer.cornerRadius = 10
+        
         
     }
 }
