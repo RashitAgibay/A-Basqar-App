a690b18d5c4c46e36a4bb3dcf1521c2f4b9049b0
Ilyas Shomat <ilyas.shomat@gmail.com>
2021-02-25 14:23:29 +0600

    Added Category to Movement

--- A-Basqar/Modules/Movement_Module/ViewControllers/MovementFirstCategoryVC.swift
+++ A-Basqar/Modules/Movement_Module/ViewControllers/MovementFirstCategoryVC.swift
@@ -12,161 +12,58 @@ import Alamofire
 
 class MovementFirstCategoryVC: DefaultVC {
 
-    
     @IBOutlet weak var collectionView: UICollectionView!
     
-    var categoryArray = NSArray()
+    var cats = [FirstLevelCat]()
     var categoryID = Int()
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
-        self.getCatList()
+        getCats()
     }
     
-    
+    private func getCats() {
+        ProductNetworkManager.service.getFirstLevelCats { (categories, error) in
+            self.cats = categories ?? [FirstLevelCat]()
+            self.collectionView.reloadData()
+        }
+    }
 }
 
 extension MovementFirstCategoryVC: UICollectionViewDelegate, UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        
-        return categoryArray.count
+        return cats.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
         let cell  = collectionView.dequeueReusableCell(withReuseIdentifier: "addProd", for: indexPath) as! FirstLevelCatImportCell
         
-        let singleCat = self.categoryArray[indexPath.row] as! NSDictionary
-        let category = singleCat["category"] as! NSDictionary
-                
-        let catName = category["name"] as! String
-        cell.catNameLabel.text = catName
-
-        if category["goods_cat_image"] != nil {
-            
-            let catImageUrl = category["goods_cat_image"] as! String
-            
-            cell.catImageView.sd_setImage(with: URL(string: catImageUrl), placeholderImage: UIImage(named: "img1"))
-        }
-        
+        let currentCat = cats[indexPath.row]
+        cell.catNameLabel.text = currentCat.name
         
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        
-        let singleCat = self.categoryArray[indexPath.row] as! NSDictionary
-        let category = singleCat["category"] as! NSDictionary
-        let categoryID = category["id"] as! Int
-        
-        self.categoryID = categoryID
-        
+        let currentCat = cats[indexPath.row]
+        self.categoryID = currentCat.id!
         self.navigateToProductList()
     }
-    
 }
 
 extension MovementFirstCategoryVC {
     
     private func navigateToProductList() {
-        
         performSegue(withIdentifier: "fromFirstLevelCatToProdList", sender: self)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
         if segue.identifier == "fromFirstLevelCatToProdList" {
             if let navigationVC = segue.destination as? UINavigationController,
                 let destVC = navigationVC.topViewController as? ProductListImportVC {
                 destVC.categoryID = self.categoryID
             }
         }
-    }
-}
-
-extension MovementFirstCategoryVC {
-    
-    private func getCatList() {
-        
-        do {
-            
-            self.reachability = try Reachability.init()
-        
-        }
-        
-        catch {
-        
-        }
-        
-        if ((reachability?.connection) != .unavailable){
-            MBProgressHUD.showAdded(to: self.view, animated: true)
-            
-            //MARK: - Токенді optional түрден String типіне алып келу керек, әйтпесе токен дұрыс жіберілмейді.
-            let token = UserDefaults.standard.string(forKey: userTokenKey) as! String
-            
-            
-            let headers: HTTPHeaders = [
-                
-                "Content-Type": "application/json".trimmingCharacters(in: .whitespacesAndNewlines),
-                "Authorization":"JWT \(token)".trimmingCharacters(in: .whitespacesAndNewlines),
-            ]
-            
-
-            
-            let encodeURL = firstLevelCategoryListUrl
-            
-            let requestOfApi = AF.request(encodeURL, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: headers, interceptor: nil)
-            
-            requestOfApi.responseJSON(completionHandler: {(response)-> Void in
-                
-//                print("///", response.result)
-//                print("///", response.response)
-//                print("///", response.request)
-                
-                switch response.result {
-                
-                case .success(let payload):
-                    
-                    MBProgressHUD.hide(for: self.view, animated: true)
-                    
-                    if let x = payload as? Dictionary<String,AnyObject> {
-
-                        
-                    }
-                    
-                    else {
-                        
-                        let resultValue = payload as! NSArray
-                        
-                        self.categoryArray = resultValue
-                        self.collectionView.reloadData()
-                    }
-                
-                case .failure(let error):
-                    
-                    print(error)
-                    
-                    MBProgressHUD.hide(for: self.view, animated: true)
-                    
-                    self.showErrorsAlertWithOneCancelButton(message: "Проверьте соединение с интернетом")
-                }
-            
-            })
-        
-        }
-        
-        else {
-            
-            //print("internet is not working")
-            
-            MBProgressHUD.hide(for: self.view, animated: true)
-            self.showErrorsAlertWithOneCancelButton(message: "Проверьте соединение с интернетом")
-        }
-                       
-
     }
 }
